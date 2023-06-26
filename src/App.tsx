2151e9b0130b866cc051469933552bd0beddbd78
wust_hy <wust_hy@192.168.2.100>
2023-06-26 23:40:04 +0800

    mobile-fit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./asset/logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
+        <img src={logo} className="LogoImage" alt="logo" />
+        <div className="Desc">
+          <div className="App-title">TalkForge.AI</div>
+          <div className="APP-desc ">
+            Powered by artificial intelligence that makes language learning
+            effective and easy
+          </div>
+          <div className="Btn-wrapper">
+            <div className="App-store">
+              <span
+                className="Btn-text"
+                style={{ position: "relative", left: "3.125rem", top: "2rem" }}
+              >
+                Coming soon
+              </span>
+            </div>
+            <div className="Google-play">
+              <span
+                className="Btn-text"
+                style={{
+                  position: "relative",
+                  left: "3.83375rem",
+                  top: "0.3125rem",
+                }}
+              >
+                Coming soon
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className="Footer">
+        ©2023 Immersively Inc. All rights reserved. | Contact us at
+        dev@talkforge.ai
+      </footer>
     </div>
   );
 }
